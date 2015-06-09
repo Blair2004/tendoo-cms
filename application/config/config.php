8fe668bc07fed12521588ab09df9fc5a659e6960
Carlos Johnson <carlosjohnsonluv2004@gmail.com>
2015-06-09 02:53:52 +0200

    Use index.php
    this let use set whether he want to use mod_rewrite mode.

--- application/config/config.php
+++ application/config/config.php
@@ -30,7 +30,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
