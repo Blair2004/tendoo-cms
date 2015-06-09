953f48efe2295c4f47c66ab7604d80cb91b98e00
Carlos Johnson <carlosjohnsonluv2004@gmail.com>
2015-06-09 02:37:45 +0200

    Fix Prefix for case sensitive issue
    Fix Prefix for case sensitive issue

--- application/config/config.php
+++ application/config/config.php
@@ -110,7 +110,7 @@ $config['enable_hooks'] = TRUE;
 | http://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'TENDOO_';
+$config['subclass_prefix'] = 'Tendoo';
 
 /*
 |--------------------------------------------------------------------------
