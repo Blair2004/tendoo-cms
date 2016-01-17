a1bd2e0e293b74c0f248570acce9e89b219a1ce8
ebencarljo <blair_jersyer@hotmail.fr>
2016-01-17 11:29:20 +0100

    Fix syntax error on config.ph
    Fix syntax error on config.ph

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config[ 'base_url' ]	=	((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ?  "https" : "http") .
+$config[ 'base_url' ]	=	((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ?  "https" : "http");
 $config[ 'base_url' ] 	.=  "://".$_SERVER['HTTP_HOST'];
 $config[ 'base_url' ]	.=  str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
