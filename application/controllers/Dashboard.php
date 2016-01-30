bc4a1faa23234c408b01426e69f7d7d3ac5543fe
Blair2004 <carlosjohnsonluv2004@gmail.com>
2016-01-30 18:26:40 +0100

    Update
    - Adding ViewPort meta
    - Gui checkbox bug fix

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -12,26 +12,26 @@ class Dashboard extends Tendoo_Controller {
 	 *	- or -
 	 * this controller is in other words admin dashboard.
 	 */
-	 
+
 	public function __construct()
 	{
 		parent::__construct();
 		// $this->output->enable_profiler(TRUE);
-		
+
 		// $this->output->enable_profiler(TRUE);
 		// All those variable are not required for option interface
 		// Special assets loading for dashboard
-		
+
 		// include static libraries
 		include_once( LIBPATH .'/Menu.php' );
 		include_once( LIBPATH .'/Notice.php' );
-		
-		$this->load->model( 'gui' );		
+
+		$this->load->model( 'gui' );
 		$this->load->model( 'update_model' ); // load update model @since 3.0
 		$this->load->model( 'dashboard_model' , 'dashboard' );
-		
+
 		// Loading Admin Menu
-		// this action was move to Dashboard controler instead of aside.php output file. 
+		// this action was move to Dashboard controler instead of aside.php output file.
 		// which was called every time "create_dashboard_pages" was triggered
 		$this->events->do_action( 'load_dashboard' );
 	}
@@ -53,10 +53,10 @@ class Dashboard extends Tendoo_Controller {
 			return call_user_func_array( array( $this, $page ), $params);
 		}
 		else
-		{			
+		{
 			$this->gui->load_page( $page , $params );
 		}
-	} 
+	}
 	/**
 	 * Module List and management controller
 	 *
@@ -73,9 +73,9 @@ class Dashboard extends Tendoo_Controller {
 		if( $page === 'list' )
 		{
 			$this->events->add_filter( 'gui_page_title' , function( $title ){
-				return '<section class="content-header"><h1>' . strip_tags( $title ) . ' <a class="btn btn-primary btn-sm pull-right" href="' . site_url( array( 'dashboard' , 'modules' , 'install_zip' ) ) . '">' . __( 'Upload a zip file' ) . '</a></h1></section>'; 
+				return '<section class="content-header"><h1>' . strip_tags( $title ) . ' <a class="btn btn-primary btn-sm pull-right" href="' . site_url( array( 'dashboard' , 'modules' , 'install_zip' ) ) . '">' . __( 'Upload a zip file' ) . '</a></h1></section>';
 			});
-			
+
 			$this->events->add_action( 'displays_dashboard_errors' , function(){
 				if( isset( $_GET[ 'extra' ] ) )
 				{
@@ -88,12 +88,12 @@ class Dashboard extends Tendoo_Controller {
 		else if( $page === 'install_zip' )
 		{
 			$this->events->add_filter( 'gui_page_title' , function( $title ){
-				return '<section class="content-header"><h1>' . strip_tags( $title ) . ' <a class="btn btn-primary btn-sm pull-right" href="' . site_url( array( 'dashboard' , 'modules' ) ) . '">' . __( 'Back to modules list' ) . '</a></h1></section>'; 
+				return '<section class="content-header"><h1>' . strip_tags( $title ) . ' <a class="btn btn-primary btn-sm pull-right" href="' . site_url( array( 'dashboard' , 'modules' ) ) . '">' . __( 'Back to modules list' ) . '</a></h1></section>';
 			});
 
 			if( isset( $_FILES[ 'extension_zip' ] ) )
 			{
-				$notice	=	Modules::install( 'extension_zip' );				
+				$notice	=	Modules::install( 'extension_zip' );
 				// it means that module has been installed
 				if( is_array( $notice ) )
 				{
@@ -104,22 +104,22 @@ class Dashboard extends Tendoo_Controller {
 				{
 					$this->notice->push_notice( $this->lang->line( $notice ) );
 				}
-				
+
 			}
 			$this->gui->set_title( sprintf( __( 'Add a new extension &mdash; %s' ) , get( 'core_signature' ) ) );
 			$this->load->view( 'dashboard/modules/install' );
-		}		
+		}
 		else if( $page === 'enable' )
 		{
 			/**
 			 * Module should be enabled before trigger this action
 			**/
-			
+
 			Modules::enable( $arg2 );
-			
+
 			// Enabling recently active module
 			Modules::init( 'unique' , $arg2 );
-			
+
 			// Run the action
 			$this->events->do_action( 'do_enable_module' , $arg2 );
 			redirect( array( 'dashboard' , 'modules?notice=' . $this->events->apply_filters( 'module_activation_status' , 'module-enabled' ) ) );
@@ -131,16 +131,16 @@ class Dashboard extends Tendoo_Controller {
 			});
 			//
 			$this->events->do_action( 'do_disable_module' , $arg2 );
-			
+
 			redirect( array( 'dashboard' , 'modules?notice=' . $this->events->apply_filters( 'module_disabling_status' , 'module-disabled' ) ) );
 		}
 		else if( $page === 'remove' )
 		{
 			$this->events->add_action( 'do_remove_module' , function( $arg2 ){
-				Modules::uninstall( $arg2 );				
+				Modules::uninstall( $arg2 );
 				redirect( array( 'dashboard' , 'modules?notice=module-removed' ) );
 			});
-			
+
 			$this->events->do_action( 'do_remove_module' , $arg2 );
 		}
 		else if( $page === 'extract' )
@@ -148,7 +148,7 @@ class Dashboard extends Tendoo_Controller {
 			$this->events->add_action( 'do_extract_module' , function( $arg2 ){
 				Modules::extract( $arg2 );
 			});
-			
+
 			$this->events->do_action( 'do_extract_module' , $arg2 );
 		}
 	}
@@ -175,14 +175,15 @@ class Dashboard extends Tendoo_Controller {
 			{
 				$content	=	array();
 				// loping post value
+				global $Options;
 				foreach( $_POST as $key => $value )
 				{
-					if( ! in_array( $key , array( 'gui_saver_option_namespace' , 'gui_saver_ref' , 'gui_saver_expiration_time' , 'gui_saver_use_namespace' ) ) )
+					if( ! in_array( $key , array( 'gui_saver_option_namespace' , 'gui_saver_ref' , 'gui_saver_expiration_time' , 'gui_saver_use_namespace', 'gui_delete_option_field' ) ) )
 					{
 						/**
 						 * Merge options which a supposed to be wrapped within the same array
 						**/
-						
+
 						if( $mode == 'merge' && is_array( $value ) ) {
 							$options	=	$this->options->get( $key ); 
 							$options	=	array_merge( force_array( $options ), $value );
@@ -191,24 +192,35 @@ class Dashboard extends Tendoo_Controller {
 						if( ! is_array( $_POST[ $key ] ) )
 						{
 							if( $this->input->post( 'gui_saver_use_namespace' ) === 'true' ) {
-								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $this->input->post( $key );	
+								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $this->input->post( $key );
 							}	else	{
 								if( $mode == 'merge' && is_array( $value ) ) {
 									$this->options->set( $key , $options , true );
 								} else {
 									$this->options->set( $key , $this->input->post( $key ) , true );
 								}
-							}							
+							}
 						} else {
 							if( $this->input->post( 'gui_saver_use_namespace' ) === 'true' )	{
-								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $_POST[ $key ];	
+								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $_POST[ $key ];
 							}	else	{
 								if( $mode == 'merge' && is_array( $value ) ) {
 									$this->options->set( $key , $options , true );
 								} else {
 									$this->options->set( $key, $_POST[ $key ], true );
 								}
-							}	
+							}
+						}
+					}
+					// Fix Checkbox bug, when submiting unchecked box
+					elseif( $key == 'gui_delete_option_field' ) {
+						foreach( force_array( $_POST[ 'gui_delete_option_field' ] ) as $field_to_delete ) {
+							if( $this->input->post( 'gui_saver_use_namespace' ) === 'true' ) {
+								unset( $Options[ $this->input->post( 'gui_saver_option_namespace' ) ][ $field_to_delete ] );
+								$this->options->set( $this->input->post( 'gui_saver_option_namespace' ), $Options[ $this->input->post( 'gui_saver_option_namespace' ) ] );
+							} else {
+								$this->options->delete( $field_to_delete );
+							}
 						}
 					}
 				}
@@ -224,7 +236,7 @@ class Dashboard extends Tendoo_Controller {
 		}
 		else if( $mode == 'get' ) {
 			// Since Option Module already decode JSON
-			// Fix bug 
+			// Fix bug
 			// @since 3.0.5
 			echo json_encode( $this->options->get( $_POST[ 'option_key' ] ) );
 			// echo json_decode( $this->options->get( $_POST[ 'option_key' ] ) );
@@ -240,14 +252,14 @@ class Dashboard extends Tendoo_Controller {
 					if( ! in_array( $key , array( 'gui_saver_option_namespace' , 'gui_saver_ref' , 'gui_saver_expiration_time' , 'gui_saver_use_namespace' , 'user_id' ) ) )
 					{
 						if( $mode == 'merge_user_meta' && is_array( $value ) ) {
-							$options	=	$this->options->get( $key ); 
+							$options	=	$this->options->get( $key );
 							$options	=	array_merge( force_array( $options ), $value );
 						}
 						// save only when it's not an array
 						if( ! is_array( $_POST[ $key ] ) )
 						{
 							if( $this->input->post( 'gui_saver_use_namespace' ) === 'true' ) {
-								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $this->input->post( $key );	
+								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $this->input->post( $key );
 							}	else	{
 								if( $mode == 'merge' && is_array( $value ) ) {
 									$this->options->set( $key , $options , true, $this->input->post( 'user_id' ) );
@@ -257,7 +269,7 @@ class Dashboard extends Tendoo_Controller {
 							}
 						} else {
 							if( $this->input->post( 'gui_saver_use_namespace' ) === 'true' )	{
-								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $_POST[ $key ];	
+								$content[ $key ]	=	( $mode == 'merge' ) ? $options : $_POST[ $key ];
 							} else {
 								if( $mode == 'merge' && is_array( $value ) ) {
 									$this->options->set( $key , $options , true, $this->input->post( 'user_id' ) );
@@ -300,7 +312,7 @@ class Dashboard extends Tendoo_Controller {
 			$this->load->view( 'dashboard/update/home' , array() );
 		}
 	}
-	
+
 	/**
 	 * About controller
 	 *
@@ -315,9 +327,8 @@ class Dashboard extends Tendoo_Controller {
 		$this->events-> add_filter( 'gui_page_title' , function(){ // disabling header
 			return;
 		});
-		
+
 		$this->gui->set_title( sprintf( __( 'About &mdash; %s' ) , get( 'core_signature' ) ) );
 		$this->load->view( 'dashboard/about/body' );
 	}
 }
-
--- application/views/dashboard/header.php
+++ application/views/dashboard/header.php
@@ -9,6 +9,10 @@
 <html>
 <head>
 <meta charset="utf-8">
+<!-- Add Scale for mobile devices, @since 3.0.7 -->
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- End Add Scale for mobile devices -->
 <script type="text/javascript">
 var tendoo	=	new Object;
 </script>
