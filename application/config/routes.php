09dd68db636d2b52f489d03de79761e1c9afecf7
ebencarljo <blair_jersyer@hotmail.fr>
2016-04-08 14:30:12 +0100

    New POST rules and names
    New POST rules and names

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Frontend';
-$route['404_override'] = 'Frontend';
-$route['translate_uri_dashes'] = TRUE;
+$route['default_controller'] 	= 'Frontend';
+$route['404_override'] 			= '404';
+$route['translate_uri_dashes'] 	= TRUE;
 
 // Slugs.
 // must match reserved controllers and controllers requiring installation
