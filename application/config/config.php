a49fb5f263045388af2cbc9609e86ca4aea1726f
Reconix <reconix@outlook.com>
2016-08-23 22:34:25 +0100

    Setup DB Session Table
    File sessions not working, setup db session.

--- application/config/config.php
+++ application/config/config.php
@@ -359,6 +359,8 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
+
+/*
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 60*60*3;
@@ -366,6 +368,16 @@ $config['sess_save_path'] = null;
 $config['sess_match_ip'] = false;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = false;
+*/
+
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 14400;
+$config['sess_save_path'] = 'system_sessions';
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 600;
+$config['sess_regenerate_destroy'] = FALSE;
+
 
 /*
 |--------------------------------------------------------------------------
